c7286b7d3076a55e38f46771ae9c5b0758711ba6
Matthew Barnard <contact@missingbit.co.uk>
2024-08-17 18:54:05 +0100

    added gpl-3.0-only and gpl-3.0-or-later

--- util/tmpl.go
+++ util/tmpl.go
@@ -45,6 +45,10 @@ func MatchTmpl(license string, useSPDXIDs bool) (string, error) {
 		return tmplApache, nil
 	case "mit":
 		return tmplMIT, nil
+	case "gpl-3.0-or-later":
+		return tmplGPLThreeOrLater, nil
+	case "gpl-3.0-only":
+		return tmplGPLThreeOnly, nil
 	default:
 		return "", errLicenseNotSupported
 	}
@@ -101,6 +105,28 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.`
+
+const tmplGPLThreeOrLater = `Copyright (C) {{ .Year }}  {{ .Holder }}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.`
+const tmplGPLThreeOnly = `Copyright (C) {{ .Year }}  {{ .Holder }}
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.`
 
 const tmplSPDXIDs = `{{ if .Holder }}Copyright{{ if .Year }} {{ .Year }}{{ end }} {{ .Holder }}
 {{ end }}SPDX-License-Identifier: {{ .SPDXIDs }}`
