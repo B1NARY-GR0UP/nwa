d634e9ea1cd4e7506df3d212384569129b0c3ac0
Lorain <justlorain@qq.com>
2024-08-20 18:56:11 +0800

    format tmpl

--- util/tmpl.go
+++ util/tmpl.go
@@ -106,7 +106,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.`
 
-const tmplGPLThreeOrLater = `Copyright (C) {{ .Year }}  {{ .Holder }}
+const tmplGPLThreeOrLater = `Copyright (C) {{ .Year }} {{ .Holder }}
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -115,18 +115,25 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.`
-const tmplGPLThreeOnly = `Copyright (C) {{ .Year }}  {{ .Holder }}
+along with this program. If not, see <https://www.gnu.org/licenses/>.`
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+const tmplGPLThreeOnly = `Copyright (C) {{ .Year }} {{ .Holder }}
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, version 3.
 
-You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.`
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see <https://www.gnu.org/licenses/>.`
 
 const tmplSPDXIDs = `{{ if .Holder }}Copyright{{ if .Year }} {{ .Year }}{{ end }} {{ .Holder }}
 {{ end }}SPDX-License-Identifier: {{ .SPDXIDs }}`
